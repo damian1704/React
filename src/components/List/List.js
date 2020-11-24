ab745fd26ce3b88e0929ab738ef344edb026ed08
Damian Zielonka <damianogreen@o2.pl>
2020-11-24 20:31:35 +0100

    Add modified:src

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from './Hero.js';
-
-
+import Hero from '../Hero/Hero';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png">
+        <Hero />
       </section>
     )
   }
