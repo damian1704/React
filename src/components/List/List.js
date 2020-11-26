c26d10d44a64a4b90dcd6e943d89ae72594ce19d
Damian Zielonka <damianogreen@o2.pl>
2020-11-26 21:09:50 +0100

    Add modified:src

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title}
+          imageSourceText={this.props.image}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          {column.animals}, {column.plants}, {column.minerals}
+
+        </div>
       </section>
     )
   }
 }
 
 export default List;
+
